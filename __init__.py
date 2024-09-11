24e1eba98bce7fcf07f5ac28ab6c689f4667929d
alexwtrs <77727138+alexwtrs@users.noreply.github.com>
2024-09-11 09:52:11 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template_string, render_template, jsonify
 from flask import render_template
 from flask import json
+from flask import jsonify
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
@@ -10,10 +11,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #Comm2
-
+  
 @app.route("/contact/")
 def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
+     return render_template("contact.html")
 
 @app.route('/tawarano/')
 def meteo():
@@ -27,13 +28,39 @@ def meteo():
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
 
-@app.route("/histogramme/")
-def monhistogramme():
-    return render_template("histogramme.html")
-  
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
-  
+
+@app.route("/histogramme/")
+def monhistogramme():
+    return render_template("histogramme.html")
+
+@app.route('/commits/')
+def show_commits():
+    url = 'https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits'
+    response = requests.get(url)
+    commits_data = response.json()
+    commits_minutes = []
+
+    for commit in commits_data:
+        commit_date = commit['commit']['author']['date']
+        # Appelle la fonction extract_minutes pour récupérer les minutes
+        minutes = extract_minutes(commit_date)
+        commits_minutes.append(minutes.get_json()['minutes'])  # Convertir en JSON et extraire les minutes
+    
+    return jsonify(commits_minutes=commits_minutes)
+
+@app.route('/commits_graphique/')
+def commits_graphique():
+    return render_template('commits_graphique.html')
+
+
+@app.route('/extract-minutes/<date_string>')
+def extract_minutes(date_string):
+        date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+        minutes = date_object.minute
+        return jsonify({'minutes': minutes})
+
 if __name__ == "__main__":
   app.run(debug=True)
